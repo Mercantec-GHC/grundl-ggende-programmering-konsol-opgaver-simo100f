0a7500c4a83b5aeb30199fa6fc804ac5a1e8ce8b
simo100f <simo100f@edu.mercantec.dk>
2023-10-16 11:16:51 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -2,5 +2,7 @@
 //Console.Write("Congratulations!");
 //Console.Write(" ");
 //Console.Write("You wrote your first lines of code.");
-Console.WriteLine("This is the first line.");
-Console.Write("This is the second line.");
+//Console.WriteLine("This is the first line.");
+//Console.Write("This is the second line.");
+//Console.WriteLine('b');
+Console.WriteLine('Hello World!');
