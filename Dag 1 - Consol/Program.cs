6b3fb089a551d11481f99faff571de3771dce375
simo100f <simo100f@edu.mercantec.dk>
2023-10-16 11:06:11 +0200

    part 1 complete

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
