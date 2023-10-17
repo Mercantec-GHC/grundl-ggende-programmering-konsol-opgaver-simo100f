0101621829ee2f8795958efe22d427bb0dbb0622
simo100f <simo100f@edu.mercantec.dk>
2023-10-17 13:11:42 +0200

    Update Program.cs

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -111,14 +111,115 @@ fraudulentOrderIDs[2] = "C789";
 // fraudulentOrderIDs[3] = "D000";
 */
 
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+//string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+//Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+//Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+//Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-fraudulentOrderIDs[0] = "F000";
+//fraudulentOrderIDs[0] = "F000";
 
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+//Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+//Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+//string[] names = { "Rowena", "Robin", "Bao" };
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//int[] inventory = { 200, 450, 700, 175, 250 };
+//int sum = 0;
+//int bin = 0;
+
+//foreach (int items in inventory)
+//{
+//    sum += items;
+//    bin++;
+//    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+//}
+
+//Console.WriteLine($"We have {sum} items in inventory.");
+
+//////string[] fraudulentorderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+//////string[] fraudulentOrderIDsWithB = new string[5];
+//////int numberOfFraudulentIDsWithB = 0;
+
+//////foreach(string ID in fraudulentorderIDs)
+//////{
+//////    if (ID.StartsWith("B"))
+//////    {
+//////        fraudulentOrderIDsWithB[numberOfFraudulentIDsWithB] = ID;
+//////        numberOfFraudulentIDsWithB++;
+//////    }
+//////}
+//////foreach (string ID in fraudulentOrderIDsWithB)
+//////{
+//////    Console.WriteLine(ID);
+//////}
+
+/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+
+//Random random = new Random();
+//string[] orderIDs = new string[5];
+
+//for (int i = 0; i < orderIDs.Length; i++)
+//{
+//    int prefixValue = random.Next(65, 70);
+//    string prefix = Convert.ToChar(prefixValue).ToString();
+//    string suffix = random.Next(1, 1000).ToString("000");
+
+//    orderIDs[i] = prefix + suffix;
+//}
+
+//foreach (var orderID in orderIDs)
+//{
+//    Console.WriteLine(orderID);
+//}
+
+// Example 1:
+//Console
+//.
+//WriteLine
+//(
+//"Hello Example 1!"
+//)
+//;
+
+//// Example 2:
+//string firstWord = "Hello"; string lastWord = "Example 2"; Console.WriteLine(firstWord + " " + lastWord + "!");
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
