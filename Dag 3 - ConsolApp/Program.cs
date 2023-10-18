704782dc62b72006d527de573f2db52e5b5316ff
simo100f <simo100f@edu.mercantec.dk>
2023-10-18 10:49:26 +0200

    Update Program.cs

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -18,6 +18,46 @@
 //Console.WriteLine(pangram.Contains("fox"));
 //Console.WriteLine(pangram.Contains("cow"));
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
+//string pangram = "The quick brown fox jumps over the lazy dog.";
+//Console.WriteLine(!pangram.Contains("fox"));
+//Console.WriteLine(!pangram.Contains("cow"));
+
+//int saleAmount = 1001;
+//// int discount = saleAmount > 1000 ? 100 : 50;
+
+//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+//Random number = new Random(2);
+//int coin = number.Next();
+
+//Console.WriteLine($"{(coin == 0 ? "Heads" : "Tails")}");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if(level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if(permission.Contains("Manager"))
+{
+    if(level >= 20)
+    {
+        Console.WriteLine("Contact an admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
